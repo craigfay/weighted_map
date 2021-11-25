84704e1a04e9d74a78ba6528fd0783814a2a7844
craigfay <craigfay1@gmail.com>
2021-11-25 00:28:35 -0700

    Generic key

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,27 @@
 use std::collections::{HashSet, HashMap};
+use std::hash::Hash;
 use std::rc::Rc;
 
-struct WeightedMap {
-    // keys: HashSet<String>,
-    key_to_value: HashMap<Rc<String>, ()>,
-    key_to_weight: HashMap<Rc<String>, ()>,
+/// Any type that is used as a key for
+/// WeightedMap must implement this type:
+/// ```impl Key for String {}```
+pub trait Key: Eq + Hash + Clone {}
+impl Key for String {}
+
+struct WeightedMap<K: Key> {
+    key_to_value: HashMap<Rc<K>, ()>,
+    key_to_weight: HashMap<Rc<K>, ()>,
 }
 
-impl WeightedMap {
+impl<K: Key> WeightedMap<K> {
     pub fn new() -> Self {
         Self {
-            // keys: HashSet::new(),
             key_to_value: HashMap::new(),
             key_to_weight: HashMap::new(),
         }
     }
 
-    pub fn insert(&mut self, key: String) {
+    pub fn insert(&mut self, key: K) {
         let rc_key = Rc::new(key);
         self.key_to_value.insert(Rc::clone(&rc_key), ());
         self.key_to_weight.insert(Rc::clone(&rc_key), ());
